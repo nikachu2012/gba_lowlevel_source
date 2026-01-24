932edbf6ff3d4cbcd43e8f56c235c843fae07ec1
nikachu2012 <webmaster@nikachu.net>
2026-01-24 12:54:19 +0000

    [fix] フォーマットを統一

--- 1_hello_world/source/main.c
+++ 1_hello_world/source/main.c
@@ -1,17 +1,15 @@
-
 #include <gba_console.h>
-#include <gba_video.h>
+#include <gba_input.h>
 #include <gba_interrupt.h>
 #include <gba_systemcalls.h>
-#include <gba_input.h>
+#include <gba_video.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
-	consoleDemoInit();
-	iprintf("Hello World!\n");
+int main(void) {
+  consoleDemoInit();
+  iprintf("Hello World!\n");
 
-	while (1)
-		;
+  while (1)
+    ;
 }
