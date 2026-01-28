39d9c9b51df6db1a4535eab0a35250dab2c7ca2c
nikachu2012 <webmaster@nikachu.net>
2026-01-28 10:07:19 +0000

    [fix] 本に合わせて中身の修正

--- misc_convertImage/convert_image.py
+++ misc_convertImage/convert_image.py
@@ -1,9 +1,9 @@
 import argparse
 from PIL import Image
 
-parser = argparse.ArgumentParser(description="画像をプログラムに埋め込めるC言語ヘッダに変換します")
-parser.add_argument("input", help="入力の画像ファイル", default="./cursor.png")
-parser.add_argument("label", help="変数名", default="cursor")
+parser = argparse.ArgumentParser(description="画像をC言語ヘッダに変換します")
+parser.add_argument("input", help="入力の画像ファイル", default="image.png")
+parser.add_argument("label", help="変数名", default="image")
 
 args = parser.parse_args()
 
@@ -17,14 +17,17 @@ print(f"#pragma once")
 print(f"const int {args.label}DataWidth = {img.width};")
 print(f"const int {args.label}DataHeight = {img.height};")
 
-print(f"const unsigned short {args.label}Data[{img.width*img.height}] = {{")
+print(
+  f"const unsigned short {args.label}Data[{img.width*img.height}] = {{")
 print(f"    ", end="")
 
 for d in range(len(data) - 1):
-    r, g, b, a = data[d]
-    print(f"0x{((b & 0xf8) >> 3) << 10 | ((g & 0xf8) >> 3) << 5 | (r & 0xf8) >> 3:02x}, ", end="")
+  r, g, b, a = data[d]
+  val = ((b & 0xf8) >> 3) << 10 | ((g & 0xf8) >> 3) << 5 | (r & 0xf8) >> 3
+  print(f"0x{val:02x}, ", end="")
 
 r, g, b, a = data[-1]
-print(f"0x{((b & 0xf8) >> 3) << 10 | ((g & 0xf8) >> 3) << 5 | (r & 0xf8) >> 3:02x}")
+val = ((b & 0xf8) >> 3) << 10 | ((g & 0xf8) >> 3) << 5 | (r & 0xf8) >> 3
+print(f"0x{val:02x}")
 
 print(f"}};")
